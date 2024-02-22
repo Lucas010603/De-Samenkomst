cdfbc8e43a00ae1e987f7500428bff310c19d0ff
lucas01062003 <lucas.hermus@student.gildeopleidingen.nl>
2024-02-22 21:03:22 +0100

    push

---
+++ resources/views/reservation/index.blade.php
@@ -0,0 +1,8 @@
+@extends("components.main")
+
+@section("content")
+    ibero nisi nobis, non perferendis reiciendis suscipit temporibus unde. A ab alias autem beatae commodi consectetur corporis cupiditate deleniti distinctio dolorem doloribus earum enim eos est ex fugiat fugit id inventore itaque iusto, laboriosam laudantium maxime nemo obcaecati officia omnis pariatur perspiciatis quisquam repellat temporibus unde velit vitae voluptatum. Culpa in ipsa optio perferendis quidem? A ab accusantium aspernatur autem consequuntur cum cumque dicta eligendi enim fugit harum iste itaque iure minus, nam neque officiis optio repellat sit soluta veniam voluptatibus?
+@endsection
+
+@section('scripts')
+@endsection
