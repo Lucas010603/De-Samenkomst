aa5e9d4b7a0c49f4e22e923e5b037814872bb531
Maikel <maikel.selder@student.gildeopleidingen.nl>
2024-02-22 10:37:47 +0100

    Homescreen is finished

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -4,18 +4,47 @@
 />
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<div class="red-header">
+    <div class="logo">De Samenkomst</div>
+</div>
 <nav class="vertical-menu-wrapper">
     <div class="vertical-menu-logo">
-        <div class="logo">De Samenkomst</div>
         <span class="open-menu-btn"><hr><hr><hr></span>
     </div>
     <ul class="vertical-menu">
-        <li>Schedule</li>
-        <li>Event</li>
-        <hr />
-        <li>Setting</li>
-        <li>Privacy</li>
-        <li id="user-info">MJ<span>online</span></li>
+        <li class="menu-item">
+            <i class='bx bxs-dashboard'></i>
+            <span>Dashboard</span>
+        </li>
+        <li class="menu-item">
+            <i class='bx bxs-calendar'></i>
+            <span>Reserveringen</span>
+        </li>
+        <li class="menu-item">
+            <i class='bx bxs-add-to-queue'></i>
+            <span>Nieuwe Reservering</span>
+        </li>
+        <li class="menu-item">
+            <i class='bx bxs-user'></i>
+            <span>Klanten</span>
+        </li>
+        <li class="menu-item">
+            <i class='bx bxs-user-plus'></i>
+            <span>Klant toevoegen</span>
+        </li>
+        <li class="menu-item">
+            <i class='bx bxs-bed'></i>
+            <span>Kamers</span>
+        </li>
+        <li class="menu-item">
+            <i class='bx bxs-add-to-queue'></i>
+            <span>Kamers toevoegen</span>
+        </li>
+        <li class="menu-item">
+            <i class='bx bxs-user-detail'></i>
+            <span>Medewerkers</span>
+        </li>
+        <li id="user-info">Test user<span>online</span></li>
     </ul>
 </nav>
 <div class="content-wrapper">
@@ -26,7 +55,6 @@
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         const openMenuBtn = document.querySelector('.open-menu-btn');
-        const logo = document.querySelector('.logo');
         const body = document.body;
         const breakpoint = 768;
 
@@ -34,26 +62,22 @@
         function toggleMenu() {
             const isMenuClosed = body.classList.contains('closed-menu');
             body.classList.toggle('closed-menu', !isMenuClosed);
-            document.querySelector('.vertical-menu-logo').classList.toggle('hidden-logo', !isMenuClosed);
-            logo.style.visibility = isMenuClosed ? 'visible' : 'hidden';
         }
 
-        // Function to update logo visibility based on window width
-        function updateLogoVisibility() {
-            logo.style.visibility = window.innerWidth > breakpoint ? 'visible' : 'hidden';
+        // Function to update menu state based on window width
+        function updateMenuState() {
+            const isScreenSmall = window.innerWidth <= breakpoint;
+            body.classList.toggle('closed-menu', isScreenSmall);
         }
 
         // Initial setup
-        updateLogoVisibility();
+        updateMenuState();
 
         // Listen for window resize
         window.addEventListener('resize', function() {
-            updateLogoVisibility();
+            updateMenuState();
 
-            const isScreenSmall = window.innerWidth <= breakpoint;
-            body.classList.toggle('closed-menu', isScreenSmall);
-
-            if (isScreenSmall && !body.classList.contains('closed-menu')) {
+            if (window.innerWidth <= breakpoint && !body.classList.contains('closed-menu')) {
                 toggleMenu(); // Close menu automatically when screen size is small
             }
         });
